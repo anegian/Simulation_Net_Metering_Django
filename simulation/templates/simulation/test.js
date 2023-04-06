a504e02b94edaad2ef298231a87fc68b744938f2
Anestis Giannakosian <anestidus@hotmail.com>
2023-04-06 23:49:49 +0300

    altered panels

---
+++ simulation/templates/simulation/test.js
@@ -0,0 +1,11 @@
+function firstFunction(){
+    alert('Hi there');
+}
+
+function secondFunction(name){
+
+    alert ('Hi' + name);
+}
+
+firstFunction();
+secondFunction('Max');
---
+++ static/Net_Metering/javascript/script.js
@@ -0,0 +1,39 @@
+const slider = document.getElementById("myRangeSlider");
+const output = document.getElementById("slider-value");
+
+slider.value = 0;
+slider.disabled = 'phase_load'
+// Set the initial value of the output element to 0
+output.innerHTML = slider.value;
+
+// Used to validate that the calculator form has been filled by the user
+function validateForm() {
+
+        const powerKWValue = document.getElementById('myRangeSlider').value;
+
+        if (powerKWValue === '0') {
+            alert("Please fill out the form");
+            return false; // return false to indicate that the form was not submitted
+        }
+        return true;
+}
+
+// Listen for changes in the phase load input and store the selected option
+document.getElementById("id_select_phase").addEventListener("change", function(event){
+    slider.value = 0; // Update the slider value to 0 when the phase load is changed
+    slider.max = event.target.value === 'single_phase' ? 5 : 25
+    slider.disabled = event.target.value === 'phase_load'
+    output.innerHTML = slider.value; // Update the output element value to 0 when the phase load is changed
+})
+
+// Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
+
+// Reset the output element value to the initial value when the reset button is clicked
+    document.querySelector('button[type="reset"]').addEventListener('click', function(event) {
+        slider.disabled = "phase_load"
+        slider.value = 0;
+        output.innerHTML = slider.value;
+    });
