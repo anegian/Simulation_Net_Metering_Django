42e535c2a6c161fe20e97ed21a4abd2a1c747809
Anestis Giannakosian <anestidus@hotmail.com>
2023-04-09 18:42:03 +0300

    charts and added panel

--- static/Net_Metering/javascript/chart.js
+++ static/Net_Metering/javascript/chart.js
@@ -1,31 +1,34 @@
 // DASHBOARD CHART 1
-const cityValues = [50,60,70,80,90,100,110,120,130,140,150];
-const sunValues = [7,8,8,9,9,9,10,11,14,14,15];
+const monthValues = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC'];
+const sunValues = [900,950,1050,1100,1200,1200,1400,1350,1200,1000,950,850];
 
 new Chart("myChart1", {
   type: "line",
   data: {
-    labels: cityValues,
+    labels: monthValues,
     datasets: [{
       fill: false,
       lineTension: 0,
-      backgroundColor: "rgba(0,0,255,1.0)",
-      borderColor: "rgba(0,0,255,0.1)",
+      backgroundColor: "#A457F2",
+      borderColor: "rgba(91,186,210,0.1)",
       data: sunValues
     }]
   },
   options: {
-    title: {
+      title: {
       display: true,
-      text: "Annual Production"
+      fontColor: "#f2f2f2",
+      text: "Ετήσια Παραγωγή σε kWh",
+      fontSize: 18
     },
     legend: {display: false},
     scales: {
-      yAxes: [{ticks: {min: 6, max:16}}],
+      yAxes: [{ticks: {min: 800, max:1500, fontColor: "#f2f2f2" }}],
+      xAxes: [{ticks: {min: 0, max:12, fontColor: "#f2f2f2"}}],
     }
-    
   }
 });
+
 
 // DASHBOARD CHART 2
 const profitValues = [100,200,300,400,500,600,700,800,900,1000];
@@ -34,25 +37,39 @@ new Chart("myChart2", {
   type: "line",
   data: {
     labels: profitValues,
-    datasets: [{ 
+    datasets: [{
+      label: "City",
       data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-      borderColor: "red",
-      fill: false
+      borderColor: "#5BBAD2",
+      fill: false,
     }, { 
+      label: "Irradiance",
       data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-      borderColor: "green",
+      borderColor: "#f2f2f2",
       fill: false
     }, { 
+      label: "Profit",
       data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-      borderColor: "blue",
+      borderColor: "#A457F2",
       fill: false
     }]
   },
   options: {
       title: {
       display: true,
-      text: "Return of Investement"
-    }
+      fontColor: "#f2f2f2", 
+      text: "Return On Investement",
+      fontSize: 18
+      },
+      scales: {
+      yAxes: [{ticks: { fontColor: "#f2f2f2" }}],
+      xAxes: [{ticks: { fontColor: "#f2f2f2" }}],
+    },
+    legend: {
+      labels: {
+        fontColor: "#f2f2f2"
+      }
+    },
   }
 });
 
@@ -79,52 +96,14 @@ new Chart("myChart3", {
   options: {
     title: {
       display: true,
-      text: "Profitability"
-    }
+      fontColor: "#f2f2f2",
+      text: "Profitability",
+      fontSize: 18
+    },
+    legend: {
+      labels: {
+        fontColor: "#f2f2f2"
+      }
+    },
   }
 });
-
-
-// DASHBOARD STARTING CHART
-var ctx = document.getElementById("energy-balance-chart").getContext("2d");
-
-                // Generate the data for the chart, including the energy generation, consumption, and net balance over time
-                var generationData = [100, 150, 200, 250, 300];
-                var consumptionData = [200, 250, 300, 350, 400];
-                var netBalanceData = generationData.map(function(generation, i) {
-                  return generation - consumptionData[i];
-                });
-
-                // Create the chart using Chart.js
-                var chart = new Chart(ctx, {
-                  type: "line",
-                  data: {
-                    labels: ["Jan", "Feb", "Mar", "Apr", "May"],  // Replace with the actual labels for the time period
-                    datasets: [
-                      {
-                        label: "Generation",
-                        data: generationData,
-                        borderColor: "#3e95cd",
-                        fill: false
-                      },
-                      {
-                        label: "Consumption",
-                        data: consumptionData,
-                        borderColor: "#8e5ea2",
-                        fill: false
-                      },
-                      {
-                        label: "Net Balance",
-                        data: netBalanceData,
-                        borderColor: "#3cba9f",
-                        fill: false
-                      }
-                    ]
-                  },
-                  options: {
-                    title: {
-                      display: true,
-                      text: "Energy Generation, Consumption, and Net Balance"
-                    }
-                  }
-                });
