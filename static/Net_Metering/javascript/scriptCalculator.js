dd9fd982fd05a8270e385702ee91571f94938f46
Anestis Giannakosian <66698522+anegian@users.noreply.github.com>
2023-06-11 13:58:28 +0300

    h

--- static/Net_Metering/javascript/scriptCalculator.js
+++ static/Net_Metering/javascript/scriptCalculator.js
@@ -117,6 +117,42 @@ document.addEventListener('DOMContentLoaded', function() {
       const tiltValue = image.getAttribute('tilt');
       image.style.display = tiltValue === '30' ? 'block' : 'none';
     });
+    
+    const panels = document.getElementsByClassName("panel-calculator");
+    const previousButton = document.getElementById("previous-button");
+    const nextButton = document.getElementById("next-button");
+    let currentPanelIndex = 0;
+
+    function showPanel(index) {
+      for (let i = 0; i < panels.length; i++) {
+        if (i === index) {
+          panels[i].classList.add("active");
+        } else {
+          panels[i].classList.remove("active");
+        }
+      }
+      previousButton.disabled = index === 0;
+      nextButton.disabled = index === panels.length - 1;
+    }
+
+    function goToPreviousPanel() {
+      if (currentPanelIndex > 0) {
+        currentPanelIndex--;
+        showPanel(currentPanelIndex);
+      }
+    }
+
+    function goToNextPanel() {
+      if (currentPanelIndex < panels.length - 1) {
+        currentPanelIndex++;
+        showPanel(currentPanelIndex);
+      }
+    }
+
+    previousButton.addEventListener("click", goToPreviousPanel);
+    nextButton.addEventListener("click", goToNextPanel);
+
+    showPanel(currentPanelIndex);
   });
 
 // Listen for changes in the phase load input and store the selected option
