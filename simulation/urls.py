d2e70f5c427b8010800e72703fd5e74c0bbbee24
Anestis Giannakosian <66698522+anegian@users.noreply.github.com>
2023-07-17 10:24:38 +0300

    update july

--- simulation/urls.py
+++ simulation/urls.py
@@ -8,17 +8,10 @@ urlpatterns = [
     # path --> domain.com / simulation /
     # /my_apps --> PROJECT urls.py
 
-<<<<<<< HEAD
     path('ajax/', views.calculate_power, name='calculate_power'),
-=======
->>>>>>> 8df12127ee48ab232b6828d149c92dcbf825a3d2
     path('calculator/', views.calculator_forms_choice, name='calculator'),
     path('dashboard/', views.dashboard_results, name='dashboard'),
     path('regulations/', views.regulations, name='regulations'),
     path('about/', views.about, name='about'), 
     path('signup/', views.signup, name='signup'),
-<<<<<<< HEAD
-
-=======
->>>>>>> 8df12127ee48ab232b6828d149c92dcbf825a3d2
 ]
