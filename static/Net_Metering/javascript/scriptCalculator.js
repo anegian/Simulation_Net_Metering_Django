f41a125ca27380ba78c426c2dc3e91e3d4ea461d
Anestis Giannakosian <66698522+anegian@users.noreply.github.com>
2023-07-17 12:48:51 +0300

    checked need some more js check

--- static/Net_Metering/javascript/scriptCalculator.js
+++ static/Net_Metering/javascript/scriptCalculator.js
@@ -14,28 +14,14 @@ const panelAreaInput = document.getElementById('panelAreaInput');
 const panelCostInput = document.getElementById('panelCostInput');
 const form = document.getElementById("calculatorForm");
 // const annual_Kwh_input = document.getElementById("id_select_kwh");
-let annual_Kwh_input = document.getElementById("annual_kwh");
-// Get panel parameters
-const placeSelected = document.getElementById('regionInput');
-const latitudeInput = document.getElementById('latitude');
-const longitudeInput = document.getElementById('longitude');
-const panelParamsSelect = document.getElementById('panelParams');
-const panelWpInput = document.getElementById('panelWpInput');
-const panelEfficiencyInput = document.getElementById('panelEfficiencyInput');
-const panelAreaInput = document.getElementById('panelAreaInput');
-const panelCostInput = document.getElementById('panelCostInput');
-const form = document.getElementById("calculatorForm");
 
 const phase_load_selected = document.getElementById("id_select_phase");
 const form_submit_button = document.getElementById("submitBtn");
 const reset_button = document.getElementById("resetBtn")
 const lastPanelField = document.getElementById('lastPanelField');
-const reset_button = document.getElementById("resetBtn")
-const lastPanelField = document.getElementById('lastPanelField');
 const error_message = document.getElementById('error-message-panel');
 const error_message2 = document.getElementById('error-message-panel5');
 const error_message3 = document.getElementById('error-message-panel6');
-const max_message = document.getElementById("max-message")
 const max_message = document.getElementById("max-message")
 const storage_selection = document.getElementById("with_storage");
 const storage_kW = document.getElementById("storage_kw");
@@ -43,27 +29,6 @@ const no_storage_selection = document.getElementById("without_storage");
 // select all radio buttons with the class "form-check-input"
 const radio_buttons = document.querySelectorAll('.form-check-input');
 
-const manualPowerRadio = document.getElementById('manual_power');
-const autoPowerRadio = document.getElementById('auto_power');
-const autoPowerDiv = document.getElementById('autoPower-button-input');
-const autoPowerButton = document.getElementById('calculateProductionButton');
-
-// Get references to the radio inputs and the azimuthInput
-const radioAzimuthInputs = document.querySelectorAll('input[name="azimuth"]');
-const azimuthInput = document.getElementById('azimuthInput');
-const radioTiltInputs = document.querySelectorAll('input[name="inclination"]');
-const tiltInput = document.getElementById('tiltInput');
-const special_production_output = document.getElementById('placeProduction');
-const minimum_panels = document.getElementById('minimumPanels');
-const total_PV_area = document.getElementById('totalArea');
-
-const panels = document.getElementsByClassName("panel-calculator");
-const previousButton = document.getElementById("previous-button");
-const nextButton = document.getElementById("next-button");
-const circleLinks = document.querySelectorAll('.circle-link');
-
-// Settings for tilt images
-const images = document.querySelectorAll('.img-tilt');
 const manualPowerRadio = document.getElementById('manual_power');
 const autoPowerRadio = document.getElementById('auto_power');
 const autoPowerDiv = document.getElementById('autoPower-button-input');
@@ -177,84 +142,6 @@ radioTiltInputs.forEach(function(inputs) {
     });
 });
 
-// Show the image corresponding to 30 degrees when the page starts or reloads
-// Go to next and previous panel calculator
-document.addEventListener('DOMContentLoaded', function() {
-    tiltInput.value = '30'; // Set the value of tiltInput to 30
-    autoPowerDiv.classList.add('hidden');
-    azimuthInput.value = 0;
-    annual_Kwh_input.value = "";
-    
-    images.forEach(function(image) {
-      const tiltValue = image.getAttribute('tilt');
-      image.style.display = tiltValue === '30' ? 'block' : 'none';
-    });
-    
-    
-    let currentPanelIndex = 0;
-    
-    function showPanel(index) {
-        for (let i = 0; i < panels.length; i++) {
-          if (i === index) {
-            panels[i].classList.remove("hidden");
-          } else {
-            panels[i].classList.add("hidden");
-          }
-        }
-    }
-
-    function goToPreviousPanel() {
-      
-      if (currentPanelIndex > 0) {
-        const previousPanelIndex = currentPanelIndex - 1; // Calculate the previous panel index
-        showPanel(previousPanelIndex);
-    
-        // Update navigation active class based on panel change
-        const currentPanelLink = document.querySelector('.panel-field[href="#' + panels[currentPanelIndex].getAttribute('id') + '"]');
-        if (currentPanelLink) {
-          currentPanelLink.classList.remove('active');
-        }
-    
-        const previousId = panels[previousPanelIndex].getAttribute('id');
-        const previousPanelLink = document.querySelector('.panel-field[href="#' + previousId + '"]');
-        // Update circle links active class based on panel change
-        circleLinks[currentPanelIndex].classList.remove('active');
-    
-        console.log('Current Panel Index:', currentPanelIndex);
-        console.log('Number of Panels:', panels.length);
-    
-        if (previousPanelLink) {
-          previousPanelLink.classList.add('active');
-        } else {
-          console.error('Previous Panel Link not found:', previousId);
-        }
-    
-        currentPanelIndex = previousPanelIndex; // Update currentPanelIndex
-      }
-    }
-
-    function goToNextPanel() {
-      
-      if (currentPanelIndex < panels.length - 1) {
-        currentPanelIndex++; // Update currentPanelIndex
-        showPanel(currentPanelIndex);
-    
-        // Update circle links active class based on panel change
-        circleLinks[currentPanelIndex].classList.add('active');
-    
-        const nextPanelId = panels[currentPanelIndex].getAttribute('id');
-        const nextPanelLink = document.querySelector('.panel-field[href="#' + nextPanelId + '"]');
-        
-        
-        console.log('Current Panel Index:', currentPanelIndex+1);
-
-        if (nextPanelLink) {
-          nextPanelLink.classList.add('active');
-        } else {
-          console.error('Next Panel Link not found:', nextPanelId);
-        }
-    
-      }
 // Set default values for panel parameters
 panelWpInput.value = panelParamsSelect.options[0].value;
 panelEfficiencyInput.value = panelParamsSelect.options[0].dataset.efficiency;
@@ -441,36 +328,8 @@ annual_Kwh_input.addEventListener("input", function(){
       enableSlider();
       triggerButtonEnable();
   }
-annual_Kwh_input.addEventListener("input", function(){
-    enableSlider();
-    enableStorage();
-    let enteredValue = parseInt (annual_Kwh_input.value)
-    const maxValue = parseInt (annual_Kwh_input.max)
-    
-    if (enteredValue > maxValue) {
-        enteredValue = maxValue;
-        annual_Kwh_input.value = enteredValue;
-        max_message.style.display = "block";
-    } else {
-        max_message.style.display = "none";
-    }
-
-    if (annual_Kwh_input.value === '0' || annual_Kwh_input.value === "" ){
-      disableSlider();
-      triggerButtonDisable();
-  }else{
-      enableSlider();
-      triggerButtonEnable();
-  }
 });
 
-annual_Kwh_input.addEventListener("keypress", function(event){
-    if (isNaN(event.key) || event.key === " " ){
-        event.preventDefault();
-    }
-});
-
-slider.addEventListener("change", function(){
 annual_Kwh_input.addEventListener("keypress", function(event){
     if (isNaN(event.key) || event.key === " " ){
         event.preventDefault();
@@ -497,7 +356,6 @@ storage_kW.addEventListener("change", function(){
 });
 
 // Reset the PV_kW_output element value to the initial value when the reset button is clicked
-reset_button.addEventListener('click', function() {
 reset_button.addEventListener('click', function() {
     disableElements();
     disableErrorMessages(); 
@@ -529,7 +387,6 @@ form_submit_button.addEventListener('click', function(event) {
 // Used to validate that the calculator form has been filled by the user
 function validateForm() {
 
-    if(phase_load_selected.value === 'phase_load' ){ //&& district_average_irradiance.value === "district"){
     if(phase_load_selected.value === 'phase_load' ){ //&& district_average_irradiance.value === "district"){
         slider.classList.add('error');
         phase_load_selected.classList.add('error');
@@ -645,7 +502,6 @@ function disableSlider(){
     slider.value = 0.0;
     slider.min = 0.0;
     if (annual_Kwh_input.value === '0') {
-    if (annual_Kwh_input.value === '0') {
         slider.value = 0.0;
       } else {
         console.log("Minimum PV system's kWp: ", slider.value);
@@ -703,10 +559,7 @@ function disableErrorMessages() {
     max_message.style.display = 'none';
     max_message.style.display = 'none';
 }         
-
-
 function check_selection_kwh_conditions(){
-    if (phase_load_selected.value !== 'phase_load' && placeSelected.value !== ""){
     if (phase_load_selected.value !== 'phase_load' && placeSelected.value !== ""){
         enableAnnualkWh();
         disableErrorMessages();
@@ -716,7 +569,6 @@ function check_selection_kwh_conditions(){
         disableStorage();
     }
 }
-
 
 // Settings for theme toggler
 const themeSlider = document.getElementById('theme-slider');
@@ -779,7 +631,6 @@ function toggleAutoPowerDiv() {
     autoPowerDiv.style.display = 'block'; // Show the div
   }
 }
-
 
 function calculateAutoPower(event) {
   event.preventDefault(); // Prevent the default form submission behavior
@@ -899,9 +750,4 @@ function updateSubmitButton() {
 
 noDiscountRadio.addEventListener('change', updateSubmitButton);
 discountRadio.addEventListener('change', updateSubmitButton);
-
-
-
-
-
 
